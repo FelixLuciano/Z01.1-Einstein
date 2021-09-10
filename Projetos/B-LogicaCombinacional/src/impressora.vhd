87d71b6b8a56174725eaa8a304f14a35258ca798
Gustavo Oliveira <72350494+gustavoeso@users.noreply.github.com>
2021-09-09 22:24:13 -0300

    Update impressora.vhd

--- Projetos/B-LogicaCombinacional/src/impressora.vhd
+++ Projetos/B-LogicaCombinacional/src/impressora.vhd
@@ -10,6 +10,6 @@ end entity;
 architecture arch of impressora is
 
 begin
-    x <= not(SW1 and SW2) or not(SW1 and SW3) or not(SW3 and SW4) or not(SW2 and SW3) or not(SW2 and SW4);
+    x <= SW1 and SW2 or SW1 and SW3 or SW3 and SW4 or SW2 and SW3 or SW2 and SW4;
 
 end architecture;
