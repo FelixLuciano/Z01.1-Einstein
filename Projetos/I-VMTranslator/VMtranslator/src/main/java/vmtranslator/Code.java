cb037288d8f44d1413328d4b2e9ec16ac5517c4c
Luciano Felix <felix.lucianodias@gmail.com>
2021-11-28 20:21:39 -0300

    Code.writeArithmetic()

--- Projetos/I-VMTranslator/VMtranslator/src/main/java/vmtranslator/Code.java
+++ Projetos/I-VMTranslator/VMtranslator/src/main/java/vmtranslator/Code.java
@@ -58,27 +58,135 @@ public class Code {
 
         } else if (command.equals("sub")) {
             commands.add(String.format("; %d - SUB", lineCode++));
-            // IMPLEMENTAR AQUI O LAB
-            // LEMBRAR DE USAR A FUNÇÃO commands.add()!
+            commands.add("leaw $0, %A");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("decw %A");
+            commands.add("subw (%A), %D, %D");
+            commands.add("movw %D, (%A)");
+
         } else if (command.equals("neg")) {
             commands.add(String.format("; %d - NEG", lineCode++));
-
-        } else if (command.equals("eq")) {
-            commands.add(String.format("; %d - EQ", lineCode++));
-
-        } else if (command.equals("gt")) {
-            commands.add(String.format("; %d - GT", lineCode++));
-
-        } else if (command.equals("lt")) {
-            commands.add(String.format("; %d - LT", lineCode++));
+            commands.add("leaw $0, %A");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("negw %D");
+            commands.add("movw %D, (%A)");
 
         } else if (command.equals("and")) {
             commands.add(String.format("; %d - AND", lineCode++));
+            commands.add("leaw $0, %A");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("decw %A");
+            commands.add("andw (%A), %D, %D");
+            commands.add("movw %D, (%A)");
 
         } else if (command.equals("or")) {
             commands.add(String.format("; %d - OR", lineCode++));
+            commands.add("leaw $0, %A");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("decw %A");
+            commands.add("orw (%A), %D, %D");
+            commands.add("movw %D, (%A)");
 
         } else if (command.equals("not")) {
+            commands.add(String.format("; %d - NOT", lineCode++));
+            commands.add("leaw $0, %A");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("notw %D");
+            commands.add("movw %D, (%A)");
+
+        } else if (command.equals("eq")) {
+            commands.add(String.format("; %d - EQ", lineCode++));      commands.add("leaw $0, %A");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("decw %A");
+            commands.add("subw (%A), %D, %D");
+            commands.add("leaw $IFEQ, %A");
+            commands.add("je %D");
+            commands.add("nop");
+            commands.add("leaw $0, %A");
+            commands.add("movw %A, %D");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("decw %A");
+            commands.add("movw %D, (%A)");
+            commands.add("leaw $ENDEQ, %A");
+            commands.add("jmp");
+            commands.add("nop");
+            commands.add("IFEQ:");
+            commands.add("leaw $0, %A");
+            commands.add("subw %A, $1, %D");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("decw %A");
+            commands.add("movw %D, (%A)");
+            commands.add("ENDEQ:");
+
+        } else if (command.equals("gt")) {
+            commands.add(String.format("; %d - GT", lineCode++));
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("decw %A");
+            commands.add("subw (%A), %D, %D");
+            commands.add("leaw $IFEQ, %A");
+            commands.add("jg %D");
+            commands.add("nop");
+            commands.add("leaw $0, %A");
+            commands.add("movw %A, %D");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("decw %A");
+            commands.add("movw %D, (%A)");
+            commands.add("leaw $ENDEQ, %A");
+            commands.add("jmp");
+            commands.add("nop");
+            commands.add("IFEQ:");
+            commands.add("leaw $0, %A");
+            commands.add("subw %A, $1, %D");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("decw %A");
+            commands.add("movw %D, (%A)");
+            commands.add("ENDEQ:");
+
+        } else if (command.equals("lt")) {
+            commands.add(String.format("; %d - LT", lineCode++));
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("movw (%A), %D");
+            commands.add("decw %A");
+            commands.add("subw (%A), %D, %D");
+            commands.add("leaw $IFEQ, %A");
+            commands.add("jl %D");
+            commands.add("nop");
+            commands.add("leaw $0, %A");
+            commands.add("movw %A, %D");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("decw %A");
+            commands.add("movw %D, (%A)");
+            commands.add("leaw $ENDEQ, %A");
+            commands.add("jmp");
+            commands.add("nop");
+            commands.add("IFEQ:");
+            commands.add("leaw $0, %A");
+            commands.add("subw %A, $1, %D");
+            commands.add("movw (%A), %A");
+            commands.add("decw %A");
+            commands.add("decw %A");
+            commands.add("movw %D, (%A)");
+            commands.add("ENDEQ:");
 
         }
 
